1e0ced64acc7ded8d9f099811b3b3a683f97717c
Rosario <lararosario.ejercito@gmail.com>
2025-05-31 14:49:46 -0700

    Did JavaScript semi-magic

---
+++ script.js
@@ -0,0 +1,10 @@
+let MyName = "Lara";
+let MyAge = 13;
+let LikesSushi = true;
+
+function greeting(MyName) {
+    console.log("Hello", MyName);
+}
+greeting("Lara");
+
+let newName = document.getElementId("greeter").value;
